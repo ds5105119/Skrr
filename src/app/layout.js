12de7fa6447c369b3612946ec8c0255d7c931118
IIH <ds5105119@gmail.com>
2024-08-04 23:13:16 +0900

    change meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,33 @@
 import "./globals.css";
 
 export const metadata = {
-    metadataBase: new URL("https://skrrr.vercel.app"),
-
     title: "아기 키우기 시뮬례이터~★",
     description: "미래 내 아이의 직업은?",
     icons: {
         icon: "/favicon.ico",
     },
+    openGraph: {
+        images: [
+            {
+                url: "https://skrrr.vercel.app/opengraph-image.png",
+                alt: "아기 키우기 시뮬례이터~★",
+                width: 1859,
+                height: 992,
+            },
+        ],
+    },
+    twitter: {
+        images: [
+            {
+                url: "https://skrrr.vercel.app/twitter-image.png",
+                alt: "아기 키우기 시뮬례이터~★",
+                width: 1859,
+                height: 992,
+            },
+        ],
+    },
+    locale: "ko_KR",
+    type: "website",
 };
 
 export default function RootLayout({ children }) {
